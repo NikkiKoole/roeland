dc371a07108cc028d4513f61c5292cd4e9cd0ee7
Nikki Koole <nikki@floorplanner.com>
2025-12-29 17:39:36 +0100

    Ensure public folder is properly configured

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
+// Vite config for GitHub Pages deployment
 export default defineConfig(({ command }) => {
   const base = command === "build" ? "/roeland/" : "/";
 
   return {
     plugins: [svelte()],
     base,
-    publicDir: "public",
+    publicDir: "public", // Explicitly set public directory
   };
 });
